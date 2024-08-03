542fd09368ecb6627371928f84f47fa3c79b323c
John Jezon Barlisan Ajias <johnjezonajias@gmail.com>
2024-08-03 21:45:31 +0800

    Add: Timber implementation

---
+++ web/app/themes/zyrielle/functions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Timber starter-theme
+ * https://github.com/timber/starter-theme
+ */
+
+// Load Composer dependencies.
+require_once __DIR__ . '/vendor/autoload.php';
+
+require_once __DIR__ . '/src/StarterSite.php';
+
+Timber\Timber::init();
+
+// Sets the directories (inside your theme) to find .twig files.
+Timber::$dirname = [ 'templates', 'views' ];
+
+new StarterSite();
